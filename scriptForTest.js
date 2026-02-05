029465b78ed24628c3332265884e12ad1615672c
atip nasakun <atip.nasakun.work@gmail.com>
2026-02-05 12:29:52 +0700

    update script for test

--- scriptForTest.js
+++ scriptForTest.js
@@ -1,5 +1,6 @@
+const baseUrl = "http://localhost:5170";
 function sendPost(controller, body) {
-  return fetch(`http://localhost:5170/${controller}`, {
+  return fetch(`${baseUrl}/api/${controller}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -10,18 +11,17 @@ function sendPost(controller, body) {
     .then((s) => s);
 }
 function sendDelete(controller) {
-  return fetch(`http://localhost:5170/${controller}`, {
+  return fetch(`${baseUrl}/api/${controller}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
-  })
-    .then((x) => x.json())
-    .then((s) => s);
+  }).then((x) => x);
 }
-async function quizSCenario() {
+async function quizScenario() {
   await sendDelete("areas");
   await sendDelete("trucks");
+  await sendDelete("assignments/reset");
   const areaOne = await sendPost("areas", {
     requiredResource: {
       food: 200,
@@ -59,4 +59,245 @@ async function quizSCenario() {
   const assignment = await sendPost("assignments");
   console.log(assignment);
 }
-quizSCenario();
+async function notEnoughtAnyResourcesScenario() {
+  await sendDelete("areas");
+  await sendDelete("trucks");
+  await sendDelete("assignments/reset");
+  const areaOne = await sendPost("areas", {
+    requiredResource: {
+      food: 200,
+      water: 3000,
+    },
+    urgencyLevel: 5,
+    timeConstraint: 6,
+  });
+  const areaTwo = await sendPost("areas", {
+    requiredResource: {
+      medicine: 500,
+    },
+    urgencyLevel: 4,
+    timeConstraint: 4,
+  });
+  const truckOne = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 5,
+      [`A${areaTwo.id}`]: 3,
+    },
+    resources: {
+      food: 250,
+      water: 300,
+    },
+  });
+  const truckTwo = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 4,
+      [`A${areaTwo.id}`]: 2,
+    },
+    resources: {
+      medicine: 60,
+    },
+  });
+  const assignment = await sendPost("assignments");
+  console.log(assignment);
+}
+async function notEnoughtAnyTimeScenario() {
+  await sendDelete("areas");
+  await sendDelete("trucks");
+  await sendDelete("assignments/reset");
+  const areaOne = await sendPost("areas", {
+    requiredResource: {
+      food: 200,
+      water: 300,
+    },
+    urgencyLevel: 5,
+    timeConstraint: 6,
+  });
+  const areaTwo = await sendPost("areas", {
+    requiredResource: {
+      medicine: 50,
+    },
+    urgencyLevel: 4,
+    timeConstraint: 4,
+  });
+  const truckOne = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 10,
+      [`A${areaTwo.id}`]: 10,
+    },
+    resources: {
+      food: 250,
+      water: 300,
+    },
+  });
+  const truckTwo = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 10,
+      [`A${areaTwo.id}`]: 10,
+    },
+    resources: {
+      medicine: 60,
+    },
+  });
+  const assignment = await sendPost("assignments");
+  console.log(assignment);
+}
+async function mixScenario() {
+  await sendDelete("areas");
+  await sendDelete("trucks");
+  await sendDelete("assignments/reset");
+  const [areaOne, areaTwo, areaThree] = await Promise.all([
+    await sendPost("areas", {
+      requiredResource: {
+        food: 2000,
+        water: 3000,
+      },
+      urgencyLevel: 5,
+      timeConstraint: 6,
+    }),
+    await sendPost("areas", {
+      requiredResource: {
+        medicine: 50,
+      },
+      urgencyLevel: 4,
+      timeConstraint: 1,
+    }),
+    await sendPost("areas", {
+      requiredResource: {
+        medicine: 50,
+      },
+      urgencyLevel: 4,
+      timeConstraint: 5,
+    }),
+    await sendPost("areas", {
+      requiredResource: {
+        medicine: 500,
+      },
+      urgencyLevel: 4,
+      timeConstraint: 1,
+    }),
+  ]);
+  Promise.all([
+    await sendPost("trucks", {
+      timeToTravel: {
+        [`A${areaOne.id}`]: 4,
+        [`A${areaTwo.id}`]: 2,
+        [`A${areaThree.id}`]: 2,
+      },
+      resources: {
+        food: 250,
+        water: 300,
+      },
+    }),
+    await sendPost("trucks", {
+      timeToTravel: {
+        [`A${areaOne.id}`]: 4,
+        [`A${areaTwo.id}`]: 2,
+        [`A${areaThree.id}`]: 4,
+      },
+      resources: {
+        medicine: 60,
+      },
+    }),
+    await sendPost("trucks", {
+      timeToTravel: {
+        [`A${areaOne.id}`]: 10,
+        [`A${areaTwo.id}`]: 10,
+      },
+      resources: {
+        medicine: 60,
+      },
+    }),
+  ]);
+
+  const assignment = await sendPost("assignments");
+  console.log(assignment);
+}
+async function noAreaScenario() {
+  await sendDelete("areas");
+  await sendDelete("trucks");
+  await sendDelete("assignments/reset");
+  const areaOne = await sendPost("areas", {
+    requiredResource: {
+      food: 200,
+      water: 300,
+    },
+    urgencyLevel: 5,
+    timeConstraint: 6,
+  });
+  const areaTwo = await sendPost("areas", {
+    requiredResource: {
+      medicine: 50,
+    },
+    urgencyLevel: 4,
+    timeConstraint: 4,
+  });
+  await sendDelete("areas");
+  const truckOne = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 5,
+      [`A${areaTwo.id}`]: 3,
+    },
+    resources: {
+      food: 250,
+      water: 300,
+    },
+  });
+  const truckTwo = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 4,
+      [`A${areaTwo.id}`]: 2,
+    },
+    resources: {
+      medicine: 60,
+    },
+  });
+  const assignment = await sendPost("assignments");
+  console.log(assignment);
+}
+async function noTruckScenario() {
+  await sendDelete("areas");
+  await sendDelete("trucks");
+  await sendDelete("assignments/reset");
+  const areaOne = await sendPost("areas", {
+    requiredResource: {
+      food: 200,
+      water: 300,
+    },
+    urgencyLevel: 5,
+    timeConstraint: 6,
+  });
+  const areaTwo = await sendPost("areas", {
+    requiredResource: {
+      medicine: 50,
+    },
+    urgencyLevel: 4,
+    timeConstraint: 4,
+  });
+  const truckOne = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 5,
+      [`A${areaTwo.id}`]: 3,
+    },
+    resources: {
+      food: 250,
+      water: 300,
+    },
+  });
+  const truckTwo = await sendPost("trucks", {
+    timeToTravel: {
+      [`A${areaOne.id}`]: 4,
+      [`A${areaTwo.id}`]: 2,
+    },
+    resources: {
+      medicine: 60,
+    },
+  });
+  await sendDelete("trucks");
+  const assignment = await sendPost("assignments");
+  console.log(assignment);
+}
+// quizScenario();
+// notEnoughtAnyResourcesScnario();
+// notEnoughtAnyTimeScenario();
+// noAreaScenario();
+// noTruckScenario();
